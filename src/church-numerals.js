1e63e96423ff2a690aab5a1802da2ace656312a2
Disday <59779377+Disday@users.noreply.github.com>
2020-01-12 01:46:24 +0300

    1

--- src/church-numerals.js
+++ src/church-numerals.js
@@ -1,15 +1,14 @@
-const F = a => b => b 
+const F = a => b => b;
 // console.log(sec(1)(2))
-const T = a => b => a
+const T = a => b => a;
 // console.log(fir(1)(2))
 //const T = fir;
-const rev = f => a => b => f(b) (a)
-const not = p => p(F)(T) 
-const and = p => q => p(q)(p)
-const or = p => q => p(p)(q)
+const rev = f => a => b => f(b)(a);
+const not = p => p(F)(T);
+const and = p => q => p(q)(p);
+const or = p => q => p(p)(q);
 const zero = f => x => x;
-const succ = n => f => a => f( n(f)(a) )
-const num = n => n(x => x + 1)(0)
-console.log(
-num(succ(zero))
-)
+const succ = n => f => a => f(n(f)(a));
+const num = n => n(x => x + 1)(0);
+console.log(num(succ(zero)));
+1111;
