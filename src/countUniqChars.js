68b25a66eb8cd2de0cc03e305787d723958ca4a2
Disday <disdain44@yandex.ru>
2020-01-31 15:18:12 +0300

    1

---
+++ src/countUniqChars.js
@@ -0,0 +1,13 @@
+const countUniqChars = str => {
+    // const stringAsArray = str.split('');
+    // console.log(stringAsArray);
+    let resultedArray = [];
+    for (const symbol of str) {
+        if (!resultedArray.includes(symbol)) {
+            resultedArray.push(symbol);
+        }
+    }
+return resultedArray.length;
+}
+console.log(
+countUniqChars('qwdrf1v412v'));123123
