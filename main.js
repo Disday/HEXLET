65a93070b8f77637d9f7afb7164405976d067539
Temin Dmitrii <disdain44@yandex.ru>
2020-02-18 00:01:01 +0300

    1

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-console.log(
-
-4 < (5 || 3));
+const fizzBuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    }
+    if (i % 5 === 0) {
+      console.log('Buzz');
+    }
+     if (i % 3 === 0) {
+      console.log('Fizz');
+    }
+    else console.log(i);
+  }
+}
+fizzBuzz();
